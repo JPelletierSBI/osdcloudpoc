d0666a8d7e7f52662b6ad62d6b28ad81de7b1904
JPelletierSBI <jpelletiersbi@gmail.com>
2025-07-08 11:52:04 -0400

    Update fr-ca.ps1

--- fr-ca.ps1
+++ fr-ca.ps1
@@ -1,6 +1,6 @@
 #to Run, boot OSDCloudUSB, at the PS Prompt: iex (irm win11.garytown.com)
-$ScriptName = 'win11.garytown.com'
-$ScriptVersion = '24.01.12.01'
+$ScriptName = 'win11.fr-ca.sbintl.net'
+$ScriptVersion = '1.00.00.01'
 Write-Host -ForegroundColor Green "$ScriptName $ScriptVersion"
 #iex (irm functions.garytown.com) #Add custom functions used in Script Hosting in GitHub
 #iex (irm functions.osdcloud.com) #Add custom fucntions from OSDCloud
